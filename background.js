60697fe85e1599f0a28cf81b4c2636fae3951c32
Lucas Adam <lucas.adam@tapptic.com>
2020-04-25 00:50:31 +0200

    add regex and redirect

--- background.js
+++ background.js
@@ -1,11 +1,13 @@
-chrome.webRequest.onBeforeSendHeaders.addListener(
+chrome.webRequest.onBeforeRequest.addListener(
     (details) => {
-        if (details.url.includes('commentary-tracks')) {
-            details.requestHeaders = []
-            details.requestHeaders.push({name:"X-CountryCode",value:"FR"})
+        if (details.method === 'GET') {
+            var reg = new RegExp("(http[s]?:\/\/)?([^\/\s]+\/agl\/1.0\/[a-zA-Z]{2,3}\/)([a-zA-Z]{2,3})(/.*)commentary-tracks")
+            var newURL = 'https://f1tv-api.formula1.com/agl/1.0/fr/en/all_devices/global/commentary-tracks'
+            if (reg.test(details.url) && details.url !== newURL) {
+                return { redirectUrl: newURL}
+            }
         }
-        return { requestHeaders: details.requestHeaders };
     },
     {urls: ['https://*.formula1.com/*']},
-    ['blocking', 'requestHeaders']
+    ['blocking']
 );
